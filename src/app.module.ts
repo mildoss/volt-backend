a815c438407c65589da169815f8c92f391d5d3f6
mildoss <vlad.maslo.05@gmail.com>
2025-12-17 12:45:54 +0200

    feat: add ConfigModule and ProductsModule to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import {PrismaService} from "./prisma.service";
+import { ProductsModule } from './products/products.module';
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ProductsModule,
+  ],
   controllers: [],
   providers: [PrismaService],
 })
