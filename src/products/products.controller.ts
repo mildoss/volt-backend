019517a0e7e945608ada5d1ce7c9cffca63de07a
mildoss <vlad.maslo.05@gmail.com>
2025-12-18 13:18:09 +0200

    feat: add OnlyAdminGuard to restrict product creation to admin users

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,12 +1,15 @@
-import {Controller, Get, Post, Body, Param, Query} from '@nestjs/common';
+import {Controller, Get, Post, Body, Param, Query, UseGuards} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import {Product} from "@prisma/client";
+import {AuthGuard} from "@nestjs/passport";
+import {OnlyAdminGuard} from "../auth/guards/admin.guard";
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
   @Post()
   async create(@Body() createProductDto: CreateProductDto): Promise<Product> {
     return this.productsService.create(createProductDto);
