260370f6b8ea7d304ed92915972e373a02ee83b8
mildoss <vlad.maslo.05@gmail.com>
2026-01-20 21:34:45 +0200

    feat: remove OnlyAdminGuard from media upload endpoint

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,7 +1,6 @@
 import {Controller, HttpCode, Post, UploadedFile, UseGuards, UseInterceptors} from "@nestjs/common";
 import {MediaService} from "./media.service";
 import {AuthGuard} from "@nestjs/passport";
-import {OnlyAdminGuard} from "../auth/guards/admin.guard";
 import {FileInterceptor} from "@nestjs/platform-express";
 
 @Controller('media')
@@ -10,7 +9,7 @@ export class MediaController {
 
   @HttpCode(200)
   @Post()
-  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
+  @UseGuards(AuthGuard('jwt'))
   @UseInterceptors(FileInterceptor('media'))
   async uploadMediaFile(@UploadedFile() mediaFile: Express.Multer.File) {
     const result = await this.mediaService.uploadMedia(mediaFile);
