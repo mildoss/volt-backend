29d1fc052366d654e640200042b612e46b5fa131
mildoss <vlad.maslo.05@gmail.com>
2026-01-24 16:57:47 +0200

    feat: add create and delete functionalities for categories with admin guard

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
+import { AuthGuard } from '@nestjs/passport';
+import { OnlyAdminGuard } from '../auth/guards/admin.guard';
 
 @Controller('categories')
 export class CategoryController {
@@ -8,5 +18,17 @@ export class CategoryController {
   @Get()
   async getAll() {
     return this.categoryService.getAll();
+  }
+
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
+  @Post()
+  async create(@Body() dto: { name: string }) {
+    return this.categoryService.create(dto.name);
+  }
+
+  @UseGuards(AuthGuard('jwt'), OnlyAdminGuard)
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    return this.categoryService.delete(+id);
   }
 }
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 
 @Injectable()
@@ -8,6 +8,37 @@ export class CategoryService {
   async getAll() {
     return this.prisma.category.findMany({
       select: { id: true, name: true },
+    });
+  }
+
+  async create(name: string) {
+    return this.prisma.category.create({
+      data: { name },
+    });
+  }
+
+  async delete(id: number) {
+    const category = await this.prisma.category.findUnique({
+      where: { id },
+      include: {
+        _count: {
+          select: { products: true },
+        },
+      },
+    });
+
+    if (!category) {
+      throw new BadRequestException('Category not found');
+    }
+
+    if (category._count.products > 0) {
+      throw new BadRequestException(
+        `Cannot delete category. It has ${category._count.products} products.`,
+      );
+    }
+
+    return this.prisma.category.delete({
+      where: { id },
     });
   }
 }
